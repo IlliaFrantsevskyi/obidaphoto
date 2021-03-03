50760746634a7b78019e1519c4bd28a769257b59
Illia Frantsevskyi <v-ilfra@microsoft.com>
2021-03-03 02:05:31 +0200

    Almost finished v1

--- data/portfolio-works.js
+++ data/portfolio-works.js
@@ -1,36 +1,94 @@
 const works = [
     {
-        "main_photo_url": "https://res.cloudinary.com/dlyslau6l/image/upload/v1614203379/portfolio_Anna/IMG_1342_r1ik6y.jpg",
+        "main_photo_url": "https://res.cloudinary.com/dlyslau6l/image/upload/v1614559169/portfolio_Anna/IMG_1243_rxuiuf.jpg",
         "photo_urls": [
-            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614203379/portfolio_Anna/IMG_1342_r1ik6y.jpg",
-            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614203379/portfolio_Anna/IMG_1243_rv6htj.jpg",
-            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614203378/portfolio_Anna/IMG_1448_bmhur7.jpg",
-            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614203377/portfolio_Anna/IMG_1219_kkrbrg.jpg",
-            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614203377/portfolio_Anna/IMG_1161_rxu4pr.jpg"
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614559169/portfolio_Anna/IMG_1243_rxuiuf.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614559170/portfolio_Anna/IMG_1342_s3vadu.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614559169/portfolio_Anna/IMG_1161_rgonsb.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614559169/portfolio_Anna/IMG_1448_dypf1m.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614559169/portfolio_Anna/IMG_1219_qr8wrp.jpg"
         ]
     },
     {
-        "main_photo_url": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/orange-tree.jpg",
+        "main_photo_url": "https://res.cloudinary.com/dlyslau6l/image/upload/v1614558906/portfolio_Kris/IMG_5050_jxcjkm.jpg",
         "photo_urls": [
-            "https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/orange-tree.jpg",
-            "https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/submerged.jpg",
-            "https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/look-out.jpg"
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614558906/portfolio_Kris/IMG_5050_jxcjkm.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614558926/portfolio_Kris/IMG_5085_jkvvod.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614558906/portfolio_Kris/IMG_5066_m0lzg4.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614558906/portfolio_Kris/IMG_5049_kjryif.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614558905/portfolio_Kris/IMG_5018_ura0vr.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614558889/portfolio_Kris/IMG_5195_1_lvfbpo.jpg"
         ]
     },
     {
-        "main_photo_url": "https://res.cloudinary.com/dlyslau6l/image/upload/v1614217964/portfolio_Anna/IMG_1342_r1ik6y_1_vcdks5.jpg",
+        "main_photo_url": "https://res.cloudinary.com/dlyslau6l/image/upload/v1614559319/portfolio_Natali/IMG_1339_hemgra.jpg",
         "photo_urls": [
-            "https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/orange-tree.jpg",
-            "https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/submerged.jpg",
-            "https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/look-out.jpg"
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614559319/portfolio_Natali/IMG_1339_hemgra.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614559319/portfolio_Natali/IMG_1325_rha5p4.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614559372/portfolio_Natali/IMG_1155_m3htff.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614559372/portfolio_Natali/IMG_0817_jhwngi.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614559372/portfolio_Natali/IMG_1067_w0xliz.jpg"
         ]
     },
     {
-        "main_photo_url": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/submerged.jpg",
+        "main_photo_url": "https://res.cloudinary.com/dlyslau6l/image/upload/v1614559582/portfolio_Vika/IMG_2105-2_gthsmu.jpg",
         "photo_urls": [
-            "https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/orange-tree.jpg",
-            "https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/submerged.jpg",
-            "https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/look-out.jpg"
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614559582/portfolio_Vika/IMG_2105-2_gthsmu.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614559582/portfolio_Vika/IMG_2061_efrghz.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614559582/portfolio_Vika/IMG_1602_pgw34f.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614559582/portfolio_Vika/IMG_2231_li8wbw.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614559582/portfolio_Vika/IMG_1511_czalb6.jpg"
+        ]
+    },
+    {
+        "main_photo_url": "https://res.cloudinary.com/dlyslau6l/image/upload/v1614637737/portfolio_Alina/IMG_1784_rovmvz.jpg",
+        "photo_urls": [
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614637737/portfolio_Alina/IMG_1784_rovmvz.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614637737/portfolio_Alina/IMG_1807_xj6cgw.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614637737/portfolio_Alina/IMG_1688_rusozc.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614637737/portfolio_Alina/IMG_1744_oclpqz.jpg"
+        ]
+    },
+    {
+        "main_photo_url": "https://res.cloudinary.com/dlyslau6l/image/upload/v1614637615/portfolio_Kate/IMG_5097_jnzdwt.jpg",
+        "photo_urls": [
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614637615/portfolio_Kate/IMG_5097_jnzdwt.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614637615/portfolio_Kate/IMG_5205_wo63a9.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614637615/portfolio_Kate/IMG_4891_gshsdj.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614637615/portfolio_Kate/IMG_4642_dsuzal.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614637615/portfolio_Kate/IMG_5129_phd5fk.jpg"
+        ]
+    },
+    {
+        "main_photo_url": "https://res.cloudinary.com/dlyslau6l/image/upload/v1614639127/portfolio_children%20/IMG_8817_ot5vyq.jpg",
+        "photo_urls": [
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614639127/portfolio_children%20/IMG_8817_ot5vyq.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614639128/portfolio_children%20/IMG_9148_cmx7sg.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614639128/portfolio_children%20/IMG_9140_iq9awg.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614639127/portfolio_children%20/IMG_8776_pnifcg.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614639127/portfolio_children%20/IMG_8437_tsbcqm.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614639127/portfolio_children%20/IMG_8556_zx6ll2.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614639127/portfolio_children%20/IMG_8380_ojbpyy.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614639127/portfolio_children%20/IMG_8566-2_hc0mu1.jpg"
+        ]
+    },
+    {
+        "main_photo_url": "https://res.cloudinary.com/dlyslau6l/image/upload/v1614638106/portfolio_Marina/%D0%B2%D1%82%D0%BE%D1%80%D0%B0%D1%8F_el7tih.jpg",
+        "photo_urls": [
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614638106/portfolio_Marina/%D0%B2%D1%82%D0%BE%D1%80%D0%B0%D1%8F_el7tih.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614638106/portfolio_Marina/IMG_3329_lc7cz9.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614638106/portfolio_Marina/IMG_3326_zpseve.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614638106/portfolio_Marina/IMG_3373_yq5ske.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614638106/portfolio_Marina/IMG_3310_ihav5j.jpg"
+        ]
+    },
+    {
+        "main_photo_url": "https://res.cloudinary.com/dlyslau6l/image/upload/v1614639169/portfolio_Yana/IMG_2885_tsv4sf.jpg",
+        "photo_urls": [
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614639169/portfolio_Yana/IMG_2885_tsv4sf.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614639169/portfolio_Yana/IMG_2939_ve8uef.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614639169/portfolio_Yana/IMG_2776_dsqanh.jpg",
+            "https://res.cloudinary.com/dlyslau6l/image/upload/v1614639169/portfolio_Yana/IMG_2802_cjacfw.jpg"
         ]
     }
 ]
